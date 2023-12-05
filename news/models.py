cf4048cadef15f80e8aba41abbe0f56945acde93
viniciuscsc <viniciuscsc@icloud.com>
2023-12-05 15:31:58 -0300

    Req 02: cria a model User e a  migration 0002_user

--- news/models.py
+++ news/models.py
@@ -8,3 +8,13 @@ class Category(models.Model):
     # __str__ deve retornar a propriedade name da categoria criada
     def __str__(self) -> str:
         return self.name
+
+
+class User(models.Model):
+    name = models.CharField(max_length=200, blank=False)
+    email = models.EmailField(max_length=200, blank=False)
+    password = models.CharField(max_length=200, blank=False)
+    role = models.CharField(max_length=200, blank=False)
+
+    def __str__(self) -> str:
+        return self.name
