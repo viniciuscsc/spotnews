c675907039b17aadec3fe533e65fe4aa01ca2470
viniciuscsc <viniciuscsc@icloud.com>
2023-12-05 16:01:07 -0300

    Req 03: cria a model News

--- news/models.py
+++ news/models.py
@@ -1,4 +1,11 @@
 from django.db import models
+from django.core.exceptions import ValidationError
+
+
+def validar_titulo(titulo: str):
+    palavras = titulo.split()
+    if len(palavras) < 2:
+        raise ValidationError("O título deve conter pelo menos 2 palavras.")
 
 
 class Category(models.Model):
@@ -18,3 +25,15 @@ class User(models.Model):
 
     def __str__(self) -> str:
         return self.name
+
+
+class News(models.Model):
+    title = models.CharField(
+        max_length=200,
+        validators=[validar_titulo],
+    )
+    content = models.CharField(blank=False)
+    author = models.ForeignKey("User")
+    created_at = models.DateField(auto_now_add=True)
+    image = models.ImageField(blank=True)
+    categories = models.ManyToManyField("Category")
