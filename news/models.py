26dfc9d668830b70d0e68b350d388a531f70c12c
viniciuscsc <viniciuscsc@icloud.com>
2023-12-05 15:15:52 -0300

    Req 01: cria a model Category e a migration 0001_initial

--- news/models.py
+++ news/models.py
@@ -1,1 +1,10 @@
-# Iniciando o projeto
+from django.db import models
+
+
+class Category(models.Model):
+    # "name" não deve aceitar informações vazias ou maiores que 200 caracteres
+    name = models.CharField(max_length=200, blank=False)
+
+    # __str__ deve retornar a propriedade name da categoria criada
+    def __str__(self) -> str:
+        return self.name
