afc908ba89de4cae36d13fe88e746bf77d683b51
viniciuscsc <viniciuscsc@icloud.com>
2023-12-07 11:49:36 -0300

    Req 05: adiciona o path da news-details-page em news/urls.py

--- news/urls.py
+++ news/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
-from news.views import home
+from news.views import home, detalhes_noticia
 
-urlpatterns = [path("", home, name="home-page")]
+urlpatterns = [
+    path("", home, name="home-page"),
+    path("news/<int:id_noticia>/", detalhes_noticia, name="news-details-page"),
+]
