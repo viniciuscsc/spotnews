d755bef3e51a44a94edf3e0ec98c50f2378e5612
viniciuscsc <viniciuscsc@icloud.com>
2023-12-07 14:46:03 -0300

    Req 06: adiciona o path de categories-form em news/urls.py

--- news/urls.py
+++ news/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from news.views import home, detalhes_noticia
+from news.views import home, detalhes_noticia, new_category_form
 
 urlpatterns = [
     path("", home, name="home-page"),
     path("news/<int:id_noticia>/", detalhes_noticia, name="news-details-page"),
+    path("categories", new_category_form, name="categories-form")
 ]
