059d5b4d55751ce68f18893d7cfb236218aea105
viniciuscsc <viniciuscsc@icloud.com>
2023-12-07 11:44:27 -0300

    Req 05: define detalhes_noticia() em news/views.py

--- news/views.py
+++ news/views.py
@@ -6,3 +6,8 @@ from news.models import News
 def home(request):
     context = {"news": News.objects.all()}
     return render(request, "home.html", context)
+
+
+def detalhes_noticia(request, id_noticia):
+    context = {"news": News.objects.get(id=id_noticia)}
+    return render(request, "news_details.html", context)
