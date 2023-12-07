66239f6e9c263253d90a51de4c28322fab7c0cb9
viniciuscsc <viniciuscsc@icloud.com>
2023-12-07 20:52:10 -0300

    Req 08: corrige CategoryView

--- news_rest/views/category_view.py
+++ news_rest/views/category_view.py
@@ -1,8 +1,8 @@
 from rest_framework import viewsets
 from news.models import Category
-from serializers.category_serializer import CategorySerializer
+from news_rest.serializers.category_serializer import CategorySerializer
 
 
 class CategoryView(viewsets.ModelViewSet):
     queryset = Category.objects.all()
-    serializer = CategorySerializer
+    serializer_class = CategorySerializer
